ab396d09604384c513d1bc82479f2dccf8f85dd8
Tran Ngo Quoc Huy <quochuy.4work@gmail.com>
2023-11-01 12:02:13 +0700

    done using xpath with beautiful soup & lxml

---
+++ driver.py
@@ -0,0 +1,9 @@
+# class driver():
+#     def __init__(self,link):
+#         self.url = 'https://webscraper.io' + link
+#         self.name = ''
+#         self.prices = ''
+#         self.descriptions = ''
+#         self.rating = ''
+#         self.reviews = ''
+#         self.image = ''
